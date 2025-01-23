b7fae59671b1831624b98e4ce24782b9b7f68289
Pranshi Patel <patelpranshi745@gmail.com>
2025-01-23 10:00:06 +0530

    a

--- src/components/Partials/Partners.jsx
+++ src/components/Partials/Partners.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../css/Partners.css'
+import { FaRegCopyright } from "react-icons/fa6";
+
 
 const Partners = () => {
   return (
-    <div className='partners w-full h-[90vh] flex items-center justify-center flex-col gap-14'>
+    <div className='partners w-full h-[80vh] flex items-center justify-evenly flex-col gap-14 '>
       <div className="partners-top text-[4vw] sm:text-[2vw] font-semibold w-[90%] sm:w-[55vw] flex justify-between items-center px-5">
         <h1 className='partners-top'>Partnered with </h1>
         <span>|</span>
@@ -25,6 +27,15 @@ const Partners = () => {
           <button className="px-4 py-2 border border-gray-200 rounded text-sm sm:text-base text-gray-900 hover:bg-gray-50 transition-colors">
             Book a demo
           </button>
+        </div>
+      </div>
+
+      <div className="footer w-full flex justify-between items-center p-6  text-[0.9vw] text-[#575757]">
+        <h5 className='flex items-center gap-1'><FaRegCopyright />Litigrit Technologies Pvt. Ltd. All right reserved.</h5>
+        <div className="flex gap-6">
+
+          <h5>Terms</h5>
+          <h5>Privacy</h5>
         </div>
       </div>
     </div>
